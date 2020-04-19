cc4e947426da57f77ed5081588c9e05e4eb69528
Vinh Nguyen <ninjaprox@gmail.com>
2020-04-19 18:09:40 +0700

    Initial Commit

---
+++ LoaderUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoaderUI
+//
+//  Created by Vinh Nguyen on 4/19/20.
+//  Copyright © 2020 Vinh Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
