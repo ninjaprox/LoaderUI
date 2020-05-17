e276ddc93d24efa3df7e7551654a9a36f4ded628
Vinh Nguyen <ninjaprox@gmail.com>
2020-05-18 00:02:56 +0700

    LineScalePulseOut

--- LoaderUI/ContentView.swift
+++ LoaderUI/ContentView.swift
@@ -38,6 +38,7 @@ struct ContentView: View {
             }
             HStack(spacing: 20) {
                 BallBeat()
+                LineScalePulseOut()
                 BallGridBeat()
             }
         }
